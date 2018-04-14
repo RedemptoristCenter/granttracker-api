99f6815df7c1f1f2c1d8b84178aafd9d7e58a56a
David Fulton <fultonium@gmail.com>
2018-04-13 22:57:37 -0500

    added POST /client/search skeleton

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -6,4 +6,19 @@ router.get('/test', (req, res) => {
     res.send({'text': 'hello'});
 });
 
+router.post('/client/search', (req, res) => {
+    const { lastName=null, firstName=null, birthDate=null } = req.body;
+
+    res.send({
+        lastName,
+        firstName,
+        birthDate
+    });
+
+});
+
+router.get('/client/:clientId', (req, res) => {
+
+});
+
 module.exports = router;
