79e8e212a45e55767850b2be1ebc5afe2cf0c341
David Fulton <fultonium@gmail.com>
2018-04-17 01:25:48 -0500

    added db promise wrapper

--- server/routes/utils.js
+++ server/routes/utils.js
@@ -1,7 +1,19 @@
+const db = require('../db/db');
+
 module.exports.checkAuth = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
     res.status(401).send();
   }
+}
+
+module.exports.dbProm = function(qString, params) {
+  return new Promise(function(resolve, reject) {
+    db.query(qString, params, function(err, results) {
+      if (err) { return reject(err) }
+
+      return resolve(results);
+    });
+  });
 }
